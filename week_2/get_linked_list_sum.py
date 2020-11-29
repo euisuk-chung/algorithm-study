f3a492e3d98627ec89cc948ca00b7f68167b0a48
chung_es <jasuchung@gmail.com>
2020-11-29 09:09:33 +0900

    week2_updates

--- week_2/get_linked_list_sum.py
+++ week_2/get_linked_list_sum.py
@@ -17,34 +17,29 @@ class LinkedList:
 
 def get_linked_list_sum(linked_list_1, linked_list_2):
     # initialize sum value
-    sum1 = 0
-    sum2 = 0
-    cur1 = linked_list_1.head
-    cur2 = linked_list_2.head
+    sum_1 = _get_linked_list_sum(linked_list_1)
+    sum_2 = _get_linked_list_sum(linked_list_2)
 
-    # loop 1
-    while cur1 is not None:
-        sum1 += cur1.data
-        cur1 = cur1.next
+    return sum_1 + sum_2
 
-    # loop 2
-    while cur2 is not None:
-        sum2 += cur2.data
-        cur2 = cur2.next
-
-    total_sum = sum1 + sum2
-
-    return total_sum #1032
+def _get_linked_list_sum(linked_list):
+    sum = 0
+    head = linked_list.head
+    while head is not None:
+        sum = sum * 10 + head.data
+        head = head.next
+    return sum
 
 
 linked_list_1 = LinkedList(6)
 linked_list_1.append(7)
 linked_list_1.append(8)
+# 678
 
 linked_list_2 = LinkedList(3)
 linked_list_2.append(4)
 linked_list_2.append(5)
-
-# 3+4+5+6+7+8 =33
+# 345
 
 print(get_linked_list_sum(linked_list_1, linked_list_2))
+# 678 + 345 = 1023
